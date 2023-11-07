8b9f0acac8d555df6030d75f55d9bb14103e3708
OlaBekkevold <ola.k.bekkevold@gmail.com>
2023-11-07 13:26:02 +0100

    Store selected data in variables
    Store data from the provided JSON file in variables

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ window.addEventListener('load', () => {
             // Calls the API with the user's location
             const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
             console.log(base);
+            // Fetches the data from the API
+            fetch(base)
+            .then((response) => {
+                return response.json();
+            })
+            // Stores selected data from the API in variables
+            .then((data) => {
+                console.log(data);
+                const {temp} = data.main;
+                const place = data.name;
+                const {description, icon} = data.weather[0];
+                const {country} = data.sys;
+                const {temp_min, temp_max} = data.main;
+                const {speed} = data.wind;
+                
+                const iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+            })
         });
     }
 });
